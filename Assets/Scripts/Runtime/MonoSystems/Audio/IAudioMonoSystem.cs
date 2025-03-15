b8d9db9600949473ab266587d44db8973a5b8362
Colby-O <okeefeca@icloud.com>
2025-03-14 21:11:42 -0400

    Volume

--- Assets/Scripts/Runtime/MonoSystems/Audio/IAudioMonoSystem.cs
+++ Assets/Scripts/Runtime/MonoSystems/Audio/IAudioMonoSystem.cs
@@ -6,5 +6,7 @@ namespace LoJam.MonoSystem
     public interface IAudioMonoSystem : IMonoSystem
     {
         public void PlaySfX(int id);
+        public void SetVolume(float volume);
+        public float GetVolume();
     }
 }
