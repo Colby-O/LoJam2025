57234f6dbebf78882bf154972ce8f5a2248dba71
Ayan <ayansingal676@gmail.com>
2025-03-14 21:51:39 -0400

    Implemented firewall direction arrows

--- Assets/Scripts/Runtime/UI/UIPusher.cs
+++ Assets/Scripts/Runtime/UI/UIPusher.cs
@@ -14,17 +14,44 @@ public class UIPusher : MonoBehaviour
     private FirewallController _firewallController;
     private const int _maxArrows = 4; 
 
+    private int leftCount = 0;
+    private int rightCount = 0;
  
-    private readonly int[] _thresholds = { 0, 2, 4, 6, 8 };
+    private readonly int[] _thresholds = { 0, 1, 2, 3, 4 };
 
     public void UpdateDameonText()
     {
         if (_firewallController != null)
         {
-            int leftCount = _firewallController.GetDaemonCount(Side.Left);
-            int rightCount = _firewallController.GetDaemonCount(Side.Right);
+            leftCount = _firewallController.GetDaemonCount(Side.Left);
+            rightCount = _firewallController.GetDaemonCount(Side.Right);
+            //_daemonCountText.text = $"Left: {leftCount} | Right: {rightCount}";
+        }
 
-            _daemonCountText.text = $"Left: {leftCount} | Right: {rightCount}";
+        int difference = rightCount - leftCount; 
+
+        int arrowCount = 0;
+        for (int i = 0; i < _thresholds.Length; i++)
+        {
+            if (Mathf.Abs(difference) >= _thresholds[i])
+                arrowCount = i;
+            else
+                break;
+        }
+
+        
+        for (int i = 0; i < _arrowImages.Count; i++)
+        {
+            if (i < arrowCount)
+            {
+                _arrowImages[i].GetComponent<Image>().enabled = true;
+                _arrowImages[i].rectTransform.localRotation = Quaternion.Euler(0, 0, difference > 0 ? 180 : 0); 
+            }
+            else
+            {
+                _arrowImages[i].GetComponent<Image>().enabled = false;
+                
+            }
         }
     }
 
